e41af6794fffe031a909e7db8a9efee891206e2d
MohammadReza <mr.rahmatzade371@gmail.com>
2021-07-20 08:22:51 +0430

    feat create a crate lib

---
+++ src/greetings/english.rs
@@ -0,0 +1,7 @@
+
+pub fn hello() -> String {
+    "hello".to_string()
+}
+pub fn goodby() -> String {
+    "goodby".to_string()
+}
---
+++ src/greetings/french.rs
@@ -0,0 +1,7 @@
+
+pub fn hello() -> String {
+    "bonjour".to_string()
+}
+pub fn goodby() -> String {
+    "au revoir".to_string()
+}
